aef0c43660792a07e034a5bc47cf69f6f0435114
SauravMuke54 <saurav54muke@gmail.com>
2022-04-29 14:36:04 -0400

    Done

---
+++ src/random/RanDom.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package random;
+
+/**
+ *
+ * @author Gopal
+ */
+public class RanDom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Youtube y = new Youtube();
+        y.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
